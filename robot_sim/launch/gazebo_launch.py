e31f09b13bad27f6d7d9b2016c939bc33cc7dc37
Jakub Krsko <jakubkrs@outlook.sk>
2025-06-25 12:58:24 +0200

    working initial sim

---
+++ robot_sim/launch/gazebo_launch.py
@@ -0,0 +1,50 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, ExecuteProcess
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    use_sim_time = LaunchConfiguration("use_sim_time", default="false")
+
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                "use_sim_time",
+                default_value="false",
+                description="Use simulation (Gazebo) clock if true",
+            ),
+            ExecuteProcess(
+                cmd=[
+                    "ign",
+                    "gazebo",
+                    "-r",
+                    "src/robot_sim/gazebo/world.sdf",
+                ],
+            ),
+            Node(
+                package="ros_gz_bridge",
+                executable="parameter_bridge",
+                name="parameter_bridge_vel",
+                output="screen",
+                parameters=[{"use_sim_time": use_sim_time}],
+                arguments=["/cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist"],
+            ),
+            Node(
+                package="ros_gz_bridge",
+                executable="parameter_bridge",
+                name="parameter_bridge_odom",
+                output="screen",
+                parameters=[{"use_sim_time": use_sim_time}],
+                arguments=["/odom@nav_msgs/msg/Odometry@gz.msgs.Odometry"],
+            ),
+        ]
+    )
+
+
+# ros2 run turtlesim turtle_teleop_key --ros-args -r /turtle1/cmd_vel:=/cmd_vel
+# ign gazebo world.sdf
+# ros2 run ros_gz_bridge parameter_bridge /cmd_vel@geometry_msgs/msg/Twist@gz.msgs.Twist

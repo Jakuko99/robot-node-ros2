984950d975ec243b78779a07becc54fb2af80782
Jakub Krsko <jakubkrs@outlook.sk>
2025-07-11 12:47:51 +0200

    mapping with two robots, map merging does not work

--- robot_sim/launch/navigation_launch.py
+++ robot_sim/launch/navigation_launch.py
@@ -15,6 +15,22 @@ def generate_launch_description():
                 name="slam_toolbox",
                 output="screen",
                 parameters=["src/robot_sim/config/slam_toolbox_params.yaml"],
+                remappings=[("/map", "/map1")],
+            ),
+            Node(
+                package="slam_toolbox",
+                executable="async_slam_toolbox_node",
+                name="slam_toolbox",
+                output="screen",
+                parameters=["src/robot_sim/config/slam_toolbox_params1.yaml"],
+                remappings=[("/map", "/map2")],
+            ),
+            Node(
+                package="merge_map",
+                executable="merge_map",
+                output="screen",
+                parameters=[{"use_sim_time": True}],
+                remappings=[("/merge_map", "/map")],
             ),
         ]
     )

c95ffd18fa8c734655009447cd9a65b93bdd0f3c
Jakub Krško <jakubkrs@outlook.sk>
2026-02-16 12:24:49 +0000

    add launchfile for new node

---
+++ robot_network/launch/new_network_launch.py
@@ -0,0 +1,28 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    resource_dir = os.path.join("src/robot_network", "resource")
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_network",
+                executable="robot_network_v2",
+                output="screen",
+                parameters=[
+                    {
+                        "train_network": True,
+                        "network_model_path": f"{resource_dir}",
+                        "global_map_topic": "/kris_robot1/map",
+                        "goal_marker_topic": "/mapping_goals",
+                        "global_frame_id": "kris_robot1_map",
+                        "use_local_maps": False,
+                        "goal_process_interval": 2.0,
+                        "robot_spin_interval": 2.0,
+                    }
+                ],
+            ),
+        ]
+    )

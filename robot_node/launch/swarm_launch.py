4ed7535673ce7a2018d2d5afd596d9de617c12d6
Jakub Krsko <jakubkrs@outlook.sk>
2026-02-19 09:02:53 +0000

    robotic swarm means no coordinator

---
+++ robot_node/launch/swarm_launch.py
@@ -0,0 +1,35 @@
+import os
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    resource_dir = os.path.join("src/robot_node", "resource")
+    return LaunchDescription(
+        [
+            Node(
+                package="robot_node",
+                executable="robot_node",
+                name="kris_robot1_node",
+                output="screen",
+                parameters=[
+                    {
+                        "robot_name": "kris_robot1",
+                        "goal_process_interval": 5.0,
+                    }
+                ],
+            ),
+            Node(
+                package="robot_node",
+                executable="robot_node",
+                name="kris_robot2_node",
+                output="screen",
+                parameters=[
+                    {
+                        "robot_name": "kris_robot2",
+                        "goal_process_interval": 5.0,
+                    }
+                ],
+            ),
+        ]
+    )

1ac1a6f6b59d1c0e873e9c6ebf3de4ac85c83bb4
Jakub Krsko <jakubkrs@outlook.sk>
2026-04-24 08:31:07 +0000

    parameter tweak

--- run_model_training.py
+++ run_model_training.py
@@ -26,10 +26,10 @@ from launch.actions import SetLaunchConfiguration
 ONLINE_TRAINING: bool = True
 RANDOM_ENV: bool = False
 PLOT_RESULTS: bool = True
-NUM_SIMULATIONS: int = 1
+NUM_SIMULATIONS: int = 4
 SIM_PERIOD: int = 3600  # duration of each simulation run in seconds
 CHECK_INTERVAL: int = 600  # interval in seconds for checking simulation progress
-OVERLAP_THRESHOLD: float = 0.4  # threshold for ratio of overlapped vs total cells
+OVERLAP_THRESHOLD: float = 0.5  # threshold for ratio of overlapped vs total cells
 EXPLORATION_THRESHOLD: float = 0.6  # threshold for ratio of explored vs total cells
 # -------------------------
 
